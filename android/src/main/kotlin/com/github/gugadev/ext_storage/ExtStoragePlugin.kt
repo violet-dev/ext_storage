4fd8897ec2ed7195f99c96d1ed9654bd35fdc220
Bhavin Doshi <bhavin.doshi1989@gmail.com>
2022-01-05 21:02:38 +0530

    Update ExtStoragePlugin.kt

--- android/src/main/kotlin/com/github/gugadev/ext_storage/ExtStoragePlugin.kt
+++ android/src/main/kotlin/com/github/gugadev/ext_storage/ExtStoragePlugin.kt
@@ -1,5 +1,6 @@
 package com.github.gugadev.ext_storage
 
+import android.content.Context
 import androidx.annotation.NonNull
 import android.os.Environment
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -14,9 +15,11 @@ class ExtStoragePlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var context:Context
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "ext_storage")
+    context = flutterPluginBinding.applicationContext
     channel.setMethodCallHandler(this)
   }
 
@@ -27,6 +30,10 @@ class ExtStoragePlugin: FlutterPlugin, MethodCallHandler {
     else if (call.method == "getExternalStoragePublicDirectory"){
       val type = call.argument<String>("type");
       result.success(Environment.getExternalStoragePublicDirectory(type).toString());
+    }
+    else if(call.method == "getExternalFilesDir"){
+      val type = call.argument<String>("type");
+      result.success(context.getExternalFilesDir(type).toString());
     }
     else {
       result.notImplemented()
